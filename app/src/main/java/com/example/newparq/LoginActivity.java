8eaf7c2c6d0e4770b0d8403733c33f88c9cc73bf
Tabkil2019 <chepkemoitoo3@gmail.com>
2022-04-11 17:07:13 +0300

    chepke

--- app/src/main/java/com/example/newparq/LoginActivity.java
+++ app/src/main/java/com/example/newparq/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.newparq;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +15,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginActivity extends AppCompatActivity {
+//    private FirebaseAuth mAuth;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://newparq-475c8-default-rtdb.firebaseio.com/");
 //    ImageView got_reg;
 //
 //    EditText editTextEmail, editTextPassword;
@@ -23,8 +37,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//         mAuth = FirebaseAuth.getInstance();
 
-        final EditText email = findViewById(R.id.email);
+        final EditText phone= findViewById(R.id.phone);
         final EditText password = findViewById(R.id.password);
         final Button button_login = findViewById(R.id.login);
         final TextView forgotpasss = findViewById(R.id.fogetpass);
@@ -33,19 +48,53 @@ public class LoginActivity extends AppCompatActivity {
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //        set titile
+                getSupportActionBar().setTitle("Newparq");
 
-                final String emailTxt = email.getText().toString();
+                final String phoneTxt= phone.getText().toString();
                 final String passwordTxt = password.getText().toString();
 //                final String loginbtn = button_login.getText().toString();
 //                final String fogopass = forgotpasss.getText().toString();
 
 
-                if (emailTxt.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                if (phoneTxt.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
                 } else if (passwordTxt.isEmpty()) {
                     Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
 
-                } else {
+
+                } else{
+
+                    databaseReference.child("sers").addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //if email exists in db
+                            if(snapshot.hasChild(phoneTxt)){
+                                //matching password of user and match to one entered
+                                final String getPassword = snapshot.child(phoneTxt).child("password").getValue(String.class);
+
+
+
+                                if (getPassword.equals(passwordTxt)){
+                                    Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
 
                 }
 
--- app/src/main/java/com/example/newparq/RegisterActivity.java
+++ app/src/main/java/com/example/newparq/RegisterActivity.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,72 +28,124 @@ import com.google.firebase.database.ValueEventListener;
 public class RegisterActivity extends AppCompatActivity {
 
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://newparq-475c8-default-rtdb.firebaseio.com/");
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+//        set titile
+        Toast.makeText(this, "You can Now Register", Toast.LENGTH_SHORT).show();
+
 
         final EditText fullname = findViewById(R.id.fnames);
-        final EditText phoneTxt = findViewById(R.id.pnumber);
+        final EditText phone = findViewById(R.id.pnumber);
         final EditText identyNumber = findViewById(R.id.idno);
-        final EditText emaiil = findViewById(R.id.email);
-        final EditText passwordTxtt = findViewById(R.id.password);
-        final EditText conPassTxt = findViewById(R.id.conpass);
-        final Button registerUser = findViewById(R.id.register);
+        final EditText email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.password);
+        final EditText conPassword = findViewById(R.id.conpass);
+
+        final Button buttonRegister = findViewById(R.id.register);
+        final ProgressBar progressBar=findViewById(R.id.progress);
         final ImageView backTo = findViewById(R.id.back_to_login);
 
 
-        registerUser.setOnClickListener(new View.OnClickListener() {
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 final String fullnameTxt = fullname.getText().toString();
-                final String phone =phoneTxt.getText().toString();
+                final String phoneTxt = phone.getText().toString();
+
                 final String identyNumberTxt = identyNumber.getText().toString();
-                final String email= emaiil.getText().toString();
-                final String password = passwordTxtt.getText().toString();
-                final  String conpass = conPassTxt.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conpassTxt = conPassword.getText().toString();
 
-                if (fullnameTxt.isEmpty() || password.isEmpty() || phone.isEmpty() || email.isEmpty() || identyNumberTxt.isEmpty() || conpass.isEmpty()){
+
+                if (fullnameTxt.isEmpty() || passwordTxt.isEmpty() || phoneTxt.isEmpty() || emailTxt.isEmpty() || identyNumberTxt.isEmpty() || conpassTxt.isEmpty()) {
                     Toast.makeText(RegisterActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(fullnameTxt)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter full names", Toast.LENGTH_SHORT).show();
+                    fullname.setError("Full Name is required");
+                    fullname.requestFocus();
 
-                }
-                else if (!password.equals(conpass)){
+                } else if (phoneTxt.length() != 10) {
+                    Toast.makeText(RegisterActivity.this, "Please re_enter your mobile Number", Toast.LENGTH_SHORT).show();
+                    phone.setError("Mobile Number Should be 10 digits");
+                    phone.requestFocus();
+                } else if (TextUtils.isEmpty(conpassTxt)) {
+                    Toast.makeText(RegisterActivity.this, "Please comfirm your password", Toast.LENGTH_SHORT).show();
+
+                    conPassword.setError("Password confirmation is required");
+                    conPassword.requestFocus();
+                } else if (!passwordTxt.equals(conpassTxt)) {
                     Toast.makeText(RegisterActivity.this, "Password inserted not the same", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    conPassword.setError("password confirmation is required");
+                    conPassword.requestFocus();
 
-                            if(snapshot.hasChild(email)){
-                                Toast.makeText(RegisterActivity.this, "Email already registered ", Toast.LENGTH_SHORT).show();
-                            }else{
-                                databaseReference.child("users").child(email).child("fullname").setValue(fullnameTxt);
-                                databaseReference.child("users").child(email).child("phoneTxt").setValue(phone);
-                                databaseReference.child("users").child(email).child("identyNumber").setValue(identyNumberTxt);
-                                databaseReference.child("users").child(email).child("passwordTxtt").setValue(password);
+//                    clear entered password
+                    password.clearComposingText();
+                    conPassword.clearComposingText();
 
-                                Toast.makeText(RegisterActivity.this, "Registration successfull", Toast.LENGTH_SHORT).show();
-                                finish();
-
-                            }
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(emailTxt).matches()) {
+                    email.setError("Please insert a valid email");
+                    email.requestFocus();
+                    return;
+                } else if (passwordTxt.length() < 6) {
+                    password.setError("Minumum password length should be 6 charcters");
+                    password.requestFocus();
+                    return;
 
                 }
 
 
-
+//
+//                else{
+//                    prog.setVisibility(View.VISIBLE);
+//
+//                    databaseReference.child("sers").addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//
+//                            if(snapshot.hasChild(phoneTxt)){
+//                                Toast.makeText(RegisterActivity.this, "phone already registered ", Toast.LENGTH_SHORT).show();
+//
+//
+//                            }else{
+//                                databaseReference.child("sers").child(phoneTxt).child("fullname").setValue(fullnameTxt);
+//                                databaseReference.child("sers").child(phoneTxt).child("email").setValue(emailTxt);
+//                                databaseReference.child("sers").child(phoneTxt).child("identyNumber").setValue(identyNumberTxt);
+//                                databaseReference.child("sers").child(phoneTxt).child("password").setValue(passwordTxt);
+//
+////                                Toast.makeText(RegisterActivity.this, "Registration successfull", Toast.LENGTH_SHORT).show();
+////                                finish();
+//
+//                            }
+//
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError error) {
+//
+//                        }
+//                    });
+//
+//                }
+//
+//
+//
+//            }
+//        });
+                else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    registerUser(fullnameTxt, phoneTxt, identyNumberTxt, emailTxt, passwordTxt);
+                }
             }
         });
+    
+                                    
 
 
 
@@ -99,6 +159,36 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+    private void registerUser(String fullnameTxt, String phoneTxt, String identyNumberTxt, String emailTxt, String passwordTxt) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(emailTxt,passwordTxt).addOnCompleteListener(RegisterActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Registration Succesful", Toast.LENGTH_SHORT).show();
+//         the user verification code
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+//code
+                            firebaseUser.sendEmailVerification();
+
+//                            opening user profile
+                            Intent intent = new Intent(RegisterActivity.this,UserProfileActivity.class);
+                            //to prevent user from returining to register activity on pressing back button after registration
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();      //closes the register activity
+
+
+                        }
+
+
+                    }
+                });
 
 
     }
